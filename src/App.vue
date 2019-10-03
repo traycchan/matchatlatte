d23a069a8024b74b11715b863a34bd39f9fa3ca4
Whapow <joshuavancamp@gmail.com>
2019-10-02 20:41:56 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="body">
+      <HelloWorld msg="One Fine Day "/>
+      <img alt="Vue logo" src="./assets/logo.jpg">
+    </div>
+    <div class="sidebar">
+      <button-panel/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ButtonPanel from './components/ButtonPanel.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    ButtonPanel
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ButtonPanel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="button-panel">
+    <button>Text</button>
+    <button>Text</button>
+    <button>Text</button>
+  </div>
+</template>
+
+<style scoped>
+.button-panel {
+  height: 24rem;
+  margin-top: 4.75rem;
+  background-color: lime;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
